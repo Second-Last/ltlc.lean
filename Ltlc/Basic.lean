cfe311a609271d13f58c3460feb1117ab2c311fc
Gavin Zhao <gavinz@brown.edu>
2024-12-13 17:54:41 -0500

    Beta reduction on var
    Signed-off-by: Gavin Zhao <gavinz@brown.edu>

--- Ltlc/Basic.lean
+++ Ltlc/Basic.lean
@@ -16,48 +16,46 @@ inductive LtlcTerm
   | app : LtlcTerm → LtlcTerm → LtlcTerm
   deriving Repr, BEq
 
--- /- def Context := AssocList String LtlcType -/
--- /--/
--- /- instance : Membership String (AssocList String LtlcType) where -/
--- /-   mem l x := l.contains x = true -/
--- /--/
--- /- instance : Membership String Context where -/
--- /-   mem l x := l.contains x = true -/
--- /--/
--- /- def context_append (x y : Context) : Context := -/
--- /-     match y with  -/
--- /-     | .nil => x  -/
--- /-     | .cons k v ys => .cons k v (context_append x ys) -/
--- /--/
--- /- instance : Append Context where -/
--- /-   append x y := context_append x y -/
+notation "Context" => AssocList String LtlcType
 
-inductive Context 
-  | empty : Context 
-  | single : String → LtlcType → Context 
-  | combine : Context → Context → Context
-
-def Context.contains (c : Context) (x : String) : Bool :=
-  match c with 
-  | .empty => false 
-  | .single y _ => x = y 
-  | .combine l r => l.contains x || r.contains x
-
-instance : Membership String Context where 
+instance : Membership String Context where
   mem l x := l.contains x = true
 
+def context_append (x y : Context) : Context :=
+    match y with 
+    | .nil => x 
+    | .cons k v ys => .cons k v (context_append x ys)
+
+instance : Append Context where
+  append x y := context_append x y
+
+-- inductive Context 
+--   | empty : Context 
+--   | single : String → LtlcType → Context 
+--   | combine : Context → Context → Context
+-- 
+-- def Context.contains (c : Context) (x : String) : Bool :=
+--   match c with 
+--   | .empty => false 
+--   | .single y _ => x = y 
+--   | .combine l r => l.contains x || r.contains x
+-- 
+-- instance : Membership String Context where 
+--   mem l x := l.contains x = true
+
 inductive HasType : Context → LtlcTerm → LtlcType → Prop
-  | var {x τ} : HasType (.single x τ) (.var x) τ
+  | var {x τ} : HasType (.cons x τ .nil) (.var x) τ
   | lam 
     {Γ x α β body}
     (x_fresh : x ∉ Γ)
-    (v_hastype : HasType (Γ.combine (.single x α)) body β)
+    (v_hastype : HasType (Γ ++ .cons x α .nil) body β)
     : HasType Γ (LtlcTerm.lam x α body) (.arrow α β)
   | app 
     {Γ₁ Γ₂ t u α β}
     (t_is_fn : HasType Γ₁ t (.arrow α β))
     (u_is_α : HasType Γ₂ u α)
-    : HasType (Γ₁.combine Γ₂) (.app t u) β
+    (is_append : Γ = Γ₁ ++ Γ₂)
+    : HasType Γ (.app t u) β
 
 @[simp]
 def subst (x : String) (e : LtlcTerm) (a : LtlcTerm) : LtlcTerm := 
@@ -75,10 +73,15 @@ inductive Step : LtlcTerm → LtlcTerm → Prop
   | app_right {f e₁ e₂}
     : Step e₁ e₂ → Step (.app f e₁) (.app f e₂)
 
+lemma cons_is_append_single {Γ : Context} {x : String} {α : LtlcType}
+  : AssocList.cons x α Γ = Γ ++ (AssocList.cons x α AssocList.nil) := rfl
+
+lemma nil_append_is_same {Γ : Context} : AssocList.nil ++ Γ = Γ := sorry
+
 theorem subst_preserves_type {Γ₁ Γ₂ x α β e a} 
-  (p1 : HasType (.combine Γ₁ (.single x α)) e β)
+  (p1 : HasType (.cons x α Γ₁) e β)
   (p2 : HasType Γ₂ a α)
-  : HasType (.combine Γ₁ Γ₂) ([x // a] e) β :=
+  : HasType (Γ₁ ++ Γ₂) ([x // a] e) β :=
     /- match p1 with  -/
     /- | .lam b x => _ -/
     /- | @HasType.app a b c d e f g h => _ -/
@@ -88,29 +91,33 @@ theorem subst_preserves_type {Γ₁ Γ₂ x α β e a}
           by_cases heq : y = x 
           . simp [heq]
             rw [heq] at p1 
-            _
+            cases p1 with 
+            | var => 
+                rw [nil_append_is_same]
+                exact p2
           . simp [heq]
-            contradiction
-    | .lam z γ b => _ 
-    | .app f u => _
+            cases p1 with
+            | var => contradiction
+    | .lam z γ b => sorry
+    | .app f u => sorry
 
-theorem preservation {Γ τ e₁ e₂}
-  (e₁_is_τ : HasType Γ e₁ τ)
-  (e₁_steps_e₂ : Step e₁ e₂)
-  : HasType Γ e₂ τ := 
-    match e₁_steps_e₂ with 
-    | Step.app_left f₁_steps_f₂ => 
-        match e₁_is_τ with 
-        | HasType.app f₁_is_fn e_is_ty => 
-            HasType.app (preservation f₁_is_fn f₁_steps_f₂) e_is_ty
-    | Step.app_right u₁_steps_u₂ =>
-        match e₁_is_τ with 
-        | HasType.app f_is_fn u₁_is_ty => 
-            HasType.app f_is_fn (preservation u₁_is_ty u₁_steps_u₂)
-    | @Step.app_lam x α e a => 
-        match e₁_is_τ with 
-        | @HasType.app Γ₁ Γ₂ _ _ _ β f_is_fn a_is_α => 
-            match f_is_fn with 
-            | @HasType.lam _ _ _ _ _ x_fresh bty => 
-                subst_preserves_type bty a_is_α
+-- theorem preservation {Γ τ e₁ e₂}
+--   (e₁_is_τ : HasType Γ e₁ τ)
+--   (e₁_steps_e₂ : Step e₁ e₂)
+--   : HasType Γ e₂ τ := 
+--     match e₁_steps_e₂ with 
+--     | Step.app_left f₁_steps_f₂ => 
+--         match e₁_is_τ with 
+--         | HasType.app f₁_is_fn e_is_ty => 
+--             HasType.app (preservation f₁_is_fn f₁_steps_f₂) e_is_ty
+--     | Step.app_right u₁_steps_u₂ =>
+--         match e₁_is_τ with 
+--         | HasType.app f_is_fn u₁_is_ty => 
+--             HasType.app f_is_fn (preservation u₁_is_ty u₁_steps_u₂)
+--     | @Step.app_lam x α e a => 
+--         match e₁_is_τ with 
+--         | @HasType.app Γ₁ Γ₂ _ _ _ β f_is_fn a_is_α => 
+--             match f_is_fn with 
+--             | @HasType.lam _ _ _ _ _ x_fresh bty => 
+--                 subst_preserves_type bty a_is_α
     
